a8dd8789e3ba74b13e9ce33531c5aa8bf4d60952
İmran Kocabıyık <imrankocabiyik@gmail.com>
2016-03-02 11:47:38 +0200

    Adding Codes

---
+++ networkGraph.R
@@ -0,0 +1,47 @@
+# Network Graph Examples  #################################
+
+# EXAMPLE 1
+# Word adjacencies: adjacency network of common adjectives and nouns in the novel David Copperfield by Charles Dickens.
+# M. E. J. Newman, Phys. Rev. E 74, 036104 (2006).
+# http://www-personal.umich.edu/~mejn/netdata/
+
+# create a file for the work
+if(!file.exists("NetworkGraph")) {
+        dir.create("NetworkGraph")
+}
+
+# set working directory
+setwd("NetworkGraph")
+
+# download file
+url <- "http://www-personal.umich.edu/~mejn/netdata/adjnoun.zip"
+if(!file.exists("adjnoun.zip")) {
+        download.file(url, destfile="adjnoun.zip")
+}
+
+unzip("adjnoun.zip")
+
+# read gml file
+# install.packages("igraph")
+library(igraph)
+adjNoun <- read.graph("adjnoun.gml", format = c("gml"))
+
+plot(adjNoun)
+
+# EXAMPLE 2
+# Les Miserables: coappearance network of characters in the novel Les Miserables.
+# D. E. Knuth, The Stanford GraphBase: A Platform for Combinatorial Computing, Addison-Wesley, Reading, MA (1993).
+# http://www-personal.umich.edu/~mejn/netdata/
+
+# download file
+urlLesMis <- "http://www-personal.umich.edu/~mejn/netdata/lesmis.zip"
+if(!file.exists("lesmis.zip")) {
+        download.file(urlLesMis, destfile="lesmis.zip")
+}
+
+unzip("lesmis.zip")
+
+# read gml file
+lesMis <- read.graph("lesmis.gml", format = c("gml"))
+
+plot(lesMis)
